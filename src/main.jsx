70fea66082c6258949a20d4d5cedb06c35d13506
khanhvx2005 <kvuxuan789@gmail.com>
2026-07-07 22:04:49 +0700

    cấu trúc dự án

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,11 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
-
+import LoginPage from './pages/login.jsx';
+import RegisterPage from './pages/register.jsx';
+import ProductPage from './pages/product.jsx';
+import UserPage from './pages/user.jsx';
+import "./styles/global.css";
 const router = createBrowserRouter([
   {
     path: "/",
@@ -13,19 +17,19 @@ const router = createBrowserRouter([
   },
   {
     path: "/login",
-    element: <div>Login page</div>,
+    element: <LoginPage />,
   },
   {
     path: "/register",
-    element: <div>Register page</div>,
+    element: <RegisterPage />,
   },
   {
     path: "/users",
-    element: <div>User page</div>,
+    element: <UserPage />,
   },
   {
     path: "/products",
-    element: <div>Product page</div>,
+    element: <ProductPage />,
   },
 ]);
 createRoot(document.getElementById('root')).render(
